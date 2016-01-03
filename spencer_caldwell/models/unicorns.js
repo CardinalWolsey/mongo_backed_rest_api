1b259a2e9562e707984d44421bdb618bd58fc7da
Spencer Caldwell <spencer.caldwell@gmail.com>
2016-01-02 16:48:30 -0800

    added some data validation

--- spencer_caldwell/models/unicorns.js
+++ spencer_caldwell/models/unicorns.js
@@ -3,7 +3,21 @@ var mongoose = require('mongoose');
 var unicornSchema = new mongoose.Schema({
   name: String,
   species: {type: String, default: 'awesome'},
-  color: {type: String, default: 'rainbow'}
+  color: {type: String, default: 'rainbow'},
+  age: {
+    type: Number,
+    min: 0,
+    max: 2000
+  },
+  mobile_number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /(?:\d{3}|\(\d{3}\))([-\/\.])\d{3}\1\d{4}/.test(v);
+      },
+      message: 'Bitch, please! {VALUE} is not a real phone number!'
+    }
+  }
 });
 
 module.exports = mongoose.model('Unicorn', unicornSchema);
